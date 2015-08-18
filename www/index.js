c55b1d3f169351dc16820d6a3b9d3a747d2314e1
Collin Arnold <collin@collinarnold.net>
2015-08-17 21:07:34 -0700

    code review links now use the twiceover:// protocol
    so that the client can open them directly

--- www/index.js
+++ www/index.js
@@ -4,7 +4,7 @@ define([
 	Request.get('/api/reviews').then(function (response) {
 		document.querySelector('.container-inner ul').innerHTML = JSON.parse(response)
 			.map(function (review) {
-				return `<li class="review-link"><a href="/api/review/${review.rowid}">${review.title}</a></li>`;
+				return `<li class="review-link"><a href="twiceover://${location.host}/api/review/${review.rowid}">${review.title}</a></li>`;
 			}).join('\n');
 	});
 });
