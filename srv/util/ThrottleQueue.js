e2166367db5a1b311a86c91fd7b6c4af8d42a266
Collin Arnold <collin@collinarnold.net>
2015-09-08 23:19:28 -0700

    adding ThrottleQueue for notification throttling
    This implements a persisted, throttled event queue for the purposes of
    email sending. With this approach, email notifications won't be lost in
    the event of crashes or extended downtime. In a subsequent change this
    will be incorporated into the notification class.

---
+++ srv/util/ThrottleQueue.js
@@ -0,0 +1,57 @@
+
+var path = require('path');
+var sqlite3 = require('sqlite3');
+var Promise = require('bluebird');
+
+var config = require('./config.json');
+
+var proto = {
+	throttleDelay: 60000,	// 1 Minute throttle window by default
+	db: null,
+	handlerFn: null,
+	
+	// Adds the given event data to the event queue, replacing prior data with the same key if present.
+	// After throttleDelay ms this event will be returned to the given handlerFn if not replaced by newer
+	// data in the interim.
+	add: function (eventKey, eventDataJson) {
+		this.db.run('INSERT OR REPLACE INTO events (key, processTime, data) VALUES (?, ?, ?)',
+			eventKey, Date.now() + this.throttleDelay, JSON.stringify(eventDataJson));
+	},
+	
+	processQueue: function () {
+		var me = this,
+			processTime = Date.now();
+			
+		// Grab all settled events
+		me.db.allAsync('SELECT key, data FROM events WHERE processTime <= ?', processTime).then(function (rows) {
+			// Fire off the entire batch so the consumer can deduplicate further if desired
+			rows.forEach(function (row) {
+				row.data = JSON.parse(row.data);
+			});
+			if (rows.length) {
+				me.handlerFn(rows);
+			}
+		}).then(function () {
+			// Clear them from the queue
+			return me.db.run('DELETE FROM events WHERE processTime <= ?', processTime);
+		}).finally(function () {
+			// Schedule our next round
+			setTimeout(me.processQueue.bind(me), me.throttleDelay);
+		});
+	}
+};
+
+function ThrottleQueue(queueName, eventHandlerFn) {
+	var obj = Object.create(proto);
+	
+	obj.handlerFn = eventHandlerFn;
+	obj.db = new sqlite3.Database(path.join(config.reviews.path, queueName + '.db'));
+	
+	obj.db.run("CREATE TABLE events (key TEXT PRIMARY KEY, processTime INTEGER NOT NULL, data TEXT NOT NULL)", function () {
+		obj.processQueue();
+	});
+	
+	return obj;
+}
+
+module.exports = ThrottleQueue;
